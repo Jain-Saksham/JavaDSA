67ce3f796241c351f45baad14cac138ba4cff75c
Saksham Jain <53533762+Jain-Saksham@users.noreply.github.com>
2024-07-01 11:57:57 +0530

    Add files via upload

---
+++ String/src/com/strings/Output.java
@@ -0,0 +1,19 @@
+package com.strings;
+
+public class Output {
+
+	public static void main(String[] args) {
+//		System.out.println(new int[] {1,2,3});
+		
+		String name=null;
+//		System.out.println(name);
+		
+		float a=123.456f;
+//		System.out.printf("%.2f", a);
+		
+//		System.out.printf("Pie: %.3f",Math.PI);
+		
+		System.out.printf("%s, %s","Jain", "Saksham");
+	}
+
+}
---
+++ String/src/com/strings/Strings.java
@@ -0,0 +1,23 @@
+package com.strings;
+
+public class Strings {
+
+	public static void main(String[] args) {
+//		String a="123";
+//		System.out.println(a);
+//		a="456";
+//		System.out.println(a);
+		
+		String b = "asd";
+		String c="asd";
+		
+//		System.out.println(b==c);
+		
+		String d=new String("asd");
+		String e=new String("asd");
+		
+		System.out.println(d==e);
+		System.out.println(d.equals(e));
+	}
+
+}
