494755f8fafc63a4814d9f9f1dadfb1a5e1594f6
Saksham Jain <53533762+Jain-Saksham@users.noreply.github.com>
2024-07-16 22:35:56 +0530

    Add files via upload

---
+++ Stacks/src/com/stack/Stacks.java
@@ -0,0 +1,18 @@
+package com.stack;
+
+import java.util.Stack;
+
+public class Stacks {
+
+	public static void main(String[] args) {
+		Stack<Integer> st=new Stack<>();
+		st.push(1);
+		st.push(2);
+		st.push(3);
+		st.push(4);
+		
+		System.out.println(st.pop());
+
+	}
+
+}
